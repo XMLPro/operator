976134d9addbb9752cac02052ace8d65604d6f1a
Jun Yamahira <c0116278ba@edu.teu.ac.jp>
2016-12-03 18:27:10 +0900

    todo

---
+++ app/src/main/java/com/example/c0116278ba/myapplication/DBOpenHelper.java
@@ -0,0 +1,47 @@
+package com.example.c0116278ba.myapplication;
+
+/**
+ * Created by c0116278ba on 2016/12/01.
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ */
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    static final String DB_NAME = "todo.db";
+    static final int DB_VERSION = 1;
+    public static final String TABLE_NAME = "todo";
+    protected SQLiteDatabase db;
+
+    // コンストラクタ
+    public DBOpenHelper(Context context){
+        //super(context, DB_NAME, null, DB_VERSION);
+        super(context, DB_NAME, null, DB_VERSION);
+
+    }
+
+    // 作成
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        // create
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + "("+
+                        "   _id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "+
+                        "   memo TEXT NOT NULL "+
+                        "   "+
+                        ");"
+        );
+
+    }
+
+    // 更新
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME + ";");
+        onCreate(db);
+    }
+
+}
---
+++ app/src/main/java/com/example/c0116278ba/myapplication/TodoAdapter.java
@@ -0,0 +1,37 @@
+package com.example.c0116278ba.myapplication;
+
+/**
+ * Created by c0116278ba on 2016/12/01.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ */
+public class TodoAdapter {
+
+    private SQLiteDatabase db;
+    static private DBOpenHelper helper;
+
+    // コンストラクタ
+    public TodoAdapter(Context context){
+        helper = new DBOpenHelper(context);
+        db = helper.getWritableDatabase();
+    }
+
+    // リストを取得
+    public Cursor getAllList(){
+        return db.query(DBOpenHelper.TABLE_NAME, null, null, null, null, null, null);
+    }
+
+    // 追加
+    public void insert(String memo){
+        ContentValues values = new ContentValues();
+        values.put("memo", memo);
+        db.insertOrThrow(DBOpenHelper.TABLE_NAME, null, values);
+    }
+
+}
